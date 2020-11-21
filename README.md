61dc5413c9c3205319ec1d21e72029b79415c0a5
Shijing Liu <brendaliu0628@gmail.com>
2020-11-20 20:45:15 -0600

    login, logout express

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Ecommerce-fullstack
+e-commerce website with shopping cart, admin management
+
+2020-11-20
+
+With filter products, jwt authentication,
+
+```bash
+server port :5000
+npm run dev
+
+```
---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+ useTempFiles: true
+}))
+
+// routes
+const userRouter = require('./routes/userRouter')
+app.use('/user', userRouter)
+
+//Connect to mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+ useCreateIndex: true,
+ useFindAndModify: false,
+ useNewUrlParser: true,
+ useUnifiedTopology: true
+}, err => {
+ if (err) throw err;
+ console.log('connect to mongoDB...')
+})
+
+app.get('/', (req, res) => {
+ res.json({ msg: "welcome to my channel. Please subscribe for us. Thanks" })
+})
+
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+ console.log('Server is running port', PORT)
+})
