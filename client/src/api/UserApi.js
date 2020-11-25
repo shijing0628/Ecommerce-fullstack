5a11a9c0116ae635f09ac6a97fe1a909fffb1dc0
Shijing Liu <brendaliu0628@gmail.com>
2020-11-24 19:39:10 -0600

    login/register part, still get vscode header error but not effect store front

--- client/src/api/UserApi.js
+++ client/src/api/UserApi.js
@@ -3,28 +3,35 @@ import axios from 'axios'
 
 
 function UserApi(token) {
- const [isLogged, setIsLogged] = useState(false)
- const [isAdmin, setIsAdmin] = useState(false)
+  const [isLogged, setIsLogged] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false)
 
- useEffect(() => {
-  if (token) {
-   const getUser = async () => {
-    try {
-     const res = await axios.get('/user/infor', {
-      headers: { Authorization: token }
-     })
-     console.log(res.data)
-    } catch (err) {
-     return alert(err.response.data.msg)
+  useEffect(() => {
+    if (token) {
+      const getUser = async () => {
+        try {
+          const res = await axios.get('/user/infor', {
+            headers: { Authorization: token }
+          })
+
+          setIsLogged(true)
+
+          res.data.role === 1 ? setIsAdmin(true) : setIsAdmin(false)
+
+        } catch (err) {
+          alert(err.response.data.msg)
+        }
+      }
+      getUser()
     }
-   }
-  }
- }, [token])
+  }, [token])
 
- return {
-  isLogged: [isLogged, setIsLogged],
-  isAdmin: [isAdmin, setIsAdmin]
- }
+
+
+  return {
+    isLogged: [isLogged, setIsLogged],
+    isAdmin: [isAdmin, setIsAdmin]
+  }
 }
 
 export default UserApi
--- client/src/components/headers/header.js
+++ client/src/components/headers/header.js
@@ -5,9 +5,39 @@ import Close from './icons/close.svg'
 import Cart from './icons/cart.svg'
 import { Link } from 'react-router-dom'
 import Logo from '../../images/logo.png'
+import axios from 'axios'
 
 const Header = () => {
  const state = useContext(GlobalState)
+ const [isLogged, setIsLogged] = state.userAPI.isLogged;
+ const [isAdmin, setIsAdmin] = state.userAPI.isAdmin;
+
+ const adminRouter = () => {
+  return (
+   <>
+    <li><Link to="/create_product">Create Product</Link></li>
+    <li><Link to="/category">Categories</Link></li>
+   </>
+  )
+ }
+
+ const loggedRouter = () => {
+  return (
+   <>
+    <li><Link to="/history">History</Link></li>
+    <li><Link to="/" onClick={logoutUser}>Logout</Link></li>
+   </>
+  )
+ }
+
+ const logoutUser = async () => {
+
+  await axios.get('/user/logout')
+  localStorage.clear()
+  setIsAdmin(false)
+  setIsLogged(false)
+ }
+
 
  return (
   <header >
@@ -17,24 +47,31 @@ const Header = () => {
    <div className="logo">
     <h1>
      <Link to="/">
-      <img className="logo" src={Logo} alt="" />
+      {isAdmin ? 'Admin' : <img className="logo" src={Logo} alt="" />}
      </Link>
     </h1>
    </div>
    <ul>
-    <li><Link to="/">Products</Link></li>
-    <li><Link to="/login">Login/Register</Link></li>
+    <li><Link to="/"> {isAdmin ? 'Products' : 'Shop'}</Link></li>
+    {isAdmin && adminRouter()}
+    {
+     isLogged ? loggedRouter() : <li><Link to="/login">Login/Register</Link></li>
+    }
+
     <li>
      <img src={Close} alt="" width="30" className="menu" />
     </li>
    </ul>
 
-   <div className="cart-icon">
-    <span>0</span>
-    <Link to="/cart">
-     <img src={Cart} alt="" width="30" />
-    </Link>
-   </div>
+   { isAdmin ? '' :
+    <div className="cart-icon">
+     <span>0</span>
+     <Link to="/cart">
+      <img src={Cart} alt="" width="30" />
+     </Link>
+    </div>
+   }
+
   </header>
  )
 }
